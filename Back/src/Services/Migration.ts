d16ba75b8dc6571195d52777d066ebbed905c816
Douglas Florido <74428545+crascaroth@users.noreply.github.com>
2021-07-06 18:31:39 -0300

    Update Migration.ts

--- Back/src/Services/Migration.ts
+++ Back/src/Services/Migration.ts
@@ -1,44 +1,56 @@
 import { BaseDatabase } from "../Data/BaseDatabase";
 
+import dotenv from "dotenv";
+import cors from "cors";
+import express, { Express } from "express";
+
+dotenv.config();
+const app: Express = express();
+app.use(express.json());
+app.use(cors());
+
 class Migration extends BaseDatabase {
-  public up = () => {
+  public up = async () => {
+    const laboratory = async () => {
+      await this.getConnection().raw(`
+      CREATE TABLE IF NOT EXISTS wa_exam (
+        id VARCHAR(255) NOT NULL,
+        nome VARCHAR(255) NOT NULL,
+        tipo VARCHAR(255) NOT NULL,
+        status TINYINT NOT NULL,
+        PRIMARY KEY (id));
+  `);
+      await BaseDatabase.destroyConnection();
+    };
 
-    const Laboratory = async () => {
+    const exam = async () => {
       await this.getConnection().raw(`
-      CREATE TABLE IF NOT EXISTS "wa_exam" (
-        "id" VARCHAR(255) NOT NULL,
-        "nome" VARCHAR(255) NOT NULL,
-        "tipo" VARCHAR(255) NOT NULL,
-        "status" TINYINT NOT NULL,
-        PRIMARY KEY ("id"));
-  `)
-    }
-
-    const Exam = async () => {
+      CREATE TABLE IF NOT EXISTS wa_laboratory (
+        id VARCHAR(255) NOT NULL,
+        nome VARCHAR(255) NOT NULL,
+        endereco VARCHAR(255) NOT NULL,
+        status TINYINT NOT NULL,
+        PRIMARY KEY (id));
+        
+    `);
+      await BaseDatabase.destroyConnection();
+    };
+    const relation = async () => {
       await this.getConnection().raw(`
-        CREATE TABLE IF NOT EXISTS medico (
-        id VARCHAR(255) PRIMARY KEY,
-        login TEXT NOT NULL,
-        password TEXT NOT NULL,
-        fk_especialidade	VARCHAR(255) NOT NULL,
-        FOREIGN KEY (fk_especialidade) REFERENCES especialidades(id)
-        );
-    `)
-    }
-    const Relation = async () => {
-      await this.getConnection().raw(`
-      CREATE TABLE IF NOT EXISTS "wa_relation_exam_laboratory" (
-        "fk_laboratory" VARCHAR(255) NOT NULL,
-        "fk_exam" VARCHAR(255) NOT NULL,
+      CREATE TABLE IF NOT EXISTS wa_relation_exam_laboratory (
+        fk_laboratory VARCHAR(255) NOT NULL,
+        fk_exam VARCHAR(255) NOT NULL,
         FOREIGN KEY (fk_laboratory) REFERENCES wa_laboratory(id),
-        FOREIGN KEY (fk_exam) REFERENCES wa_exam(id)
-        );
-      `)
-    }
-    
-  Laboratory();
-  Exam();
-  Relation();
-  }
-  
+        FOREIGN KEY (fk_exam) REFERENCES wa_exam(id));
+        
+      `);
+      await BaseDatabase.destroyConnection();
+    };
+    laboratory();
+    exam();
+    relation();
+  };
 }
+
+const migration = new Migration();
+migration.up();
